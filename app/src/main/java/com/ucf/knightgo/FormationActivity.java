c52a0307f5ab559041a153f8a621c49149dce832
KShoults <kvnshoults@gmail.com>
2016-11-27 18:24:41 -0500

    Bluetooth sends connection type to Formation (currently empty), work started on Simulation

---
+++ app/src/main/java/com/ucf/knightgo/FormationActivity.java
@@ -0,0 +1,15 @@
+package com.ucf.knightgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormationActivity extends AppCompatActivity {
+
+    public static final String FORMATION = "com.ucf.knightgo.Formation";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formation);
+    }
+}
