0d342930ced97988fd459b7df4f4623f25e0f668
Michael Claudio <Michael Claudio>
2016-11-30 00:29:09 -0500

    Finished Knight Guide

--- app/src/main/java/com/ucf/knightgo/tutorialActivity.java
+++ app/src/main/java/com/ucf/knightgo/tutorialActivity.java
@@ -27,7 +27,8 @@ public class TutorialActivity extends AppCompatActivity {
         knights = (ImageView) findViewById(R.id.imgKnights);
 
         title.setText("Welcome to Knight GO! ");
-        info.setText("Press on the menu and learn how to play!");
+        info.setText("Learn how to play Knight GO by selecting the menu on the top-right of the screen.\n" +
+                "Here you can learn how to build your army, battle other players, and find out more about each type of Knight.");
         info.setMovementMethod(new ScrollingMovementMethod());
         battlefield.setImageResource(0);
         knights.setImageResource(0);
@@ -66,11 +67,13 @@ public class TutorialActivity extends AppCompatActivity {
                         "Then you will be able to either Host a Battleground or Search for one.\n" +
                         "Once both players have joined a battleground, each player must select their own strategy and formation of their collected knights\n\n" +
                         "Each knight has their own unique fighting pattern, so try out different strategies!\n\n" +
-                        "You will be able to view your inventory in the bottom left of the formation screen.\n" +
-                        "Users can place any type of knight as many times as they want as long as they have enough in their inventory\n" +
-                        //"except for Pegasus, which can be used only once per battle\n" +
+                        "You can view your inventory in the bottom left of the formation screen.\n" +
+                        "You can place any type of knight (other than the Pegasus) as many times as you want as long as there's enough in your inventory.\n" +
                         "After placing your formation on the battlefield, click Okay." +
                         "Once the two players are ready, the battle will commence.\n\n" +
+                        "The goal is to gain more points than your opponent. Points are earned by invading the enemy.\n" +
+                        "When one of your Knights crosses the battlefield and into the enemy's side, you gain 1 point*.\n" +
+                        "Will you take a defensive approach and keep the enemy at bay, or will you overwhelm them with a ferocious charge?\n\n" +
                         "GOOD LUCK AND HAVE FUN!");
                 knights.setImageResource(0);
                 battlefield.setImageResource(R.drawable.battlefield);
@@ -81,15 +84,24 @@ public class TutorialActivity extends AppCompatActivity {
                 info.setText("Here are descriptions of each type of Knight in Knight GO.\nNote: The rating " +
                         "values are not the exact numbers.\n\nSword\nHealth: 4/5\nDamage: 4/5\nStyle: Melee\nSpecial: None\n" +
                         "The standard fighter. Can deal and take a decent amount of damage\n\n" +
-                        "Spear\nHealth: 3/5\nDamage: 3/5\nStyle:Melee\nSpecial: Can attack enemies while standing behind an ally\n" +
+                        "Spear\nHealth: 3/5\nDamage: 3/5\nStyle: Melee\nSpecial: Can attack enemies while standing behind an ally\n" +
                         "A formidable Knight. Sacrifices some damage for utility.\n\n" +
-                        "Bow\nHealth:2/5\nDamage:3/5\nStyle:Ranged\nSpecial:None\n" +
+                        "Bow\nHealth:2/5\nDamage:3/5\nStyle: Ranged\nSpecial:None\n" +
                         "Assaults enemies from afar. Try to keep them away from the frontline.\n\n" +
-                        "Mace\nHealth: 4/5\nDamage: 3/5\nStyle:Melee\nSpecial: Deals increased damage towards Halberdiers.\n" +
+                        "Mace\nHealth: 4/5\nDamage: 3/5\nStyle: Melee\nSpecial: Deals increased damage towards Halberdiers.\n" +
                         "A brutal fighter. Great for pushing down the battlefield\n\n" +
-                        "Halberd\nHealth: 4/5\nDamage: 2/5\nStyle:Melee\nSpecial: Cleaves all enemies in an arc in front of him.\n" +
+                        "Halberd\nHealth: 4/5\nDamage: 2/5\nStyle: Melee\nSpecial: Cleaves all enemies in an arc in front of him.\n" +
                         "A devastating Knight that can deal damage to multiple enemies.\n\n" +
-                        "Axe\nHealth: 4/5\nDamage:Shield\nHealth: High\nDamage: Low\nRange: Melee\n");
+                        "Axe\nHealth: 4/5\nDamage: 3/5\nStyle: Melee\nSpecial: Deals increased damage towards Shield Knights.\n" +
+                        "Cuts through defenses. Great frontline Knights.\n\n" +
+                        "Dagger\nHealth:1/5\nDamage: 5/5\nStyle: Melee\nSpecial: Can move 2 spaces in 1 turn.\n" +
+                        "Fast and deadly. Weak towards tanky enemies\n\n" +
+                        "Crossbow\nHealth:3/5\nDamage: 2/5\nStyle: Ranged\nSpecial:None\n" +
+                        "Ranged fighter. Tougher than Bowman but deals less damage\n\n" +
+                        "Shield\nHealth: 5/5\nDamage: 1/5\nRange: Melee\nSpecial:None\n" +
+                        "The best defensive Knight. Great for defending weaker knights.\n\n" +
+                        "Pegasus\nHealth: 4/5\nDamage:4/5\nRange:Melee\nSpecial: Can fly over enemies and gives 2 points when invading enemy.\n" +
+                        "The ultimate Knight. Only one can be deployed per formation.");
                 battlefield.setImageResource(0);
                 knights.setImageResource(R.drawable.knights);
             default:
